696e7e17049758708212db76a7806f94251817d3
Arend Kooy <kooy.arend@gmail.com>
2025-06-03 14:28:22 +0200

    Created a class for field with init function and calculate cells function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # blabla
+
+
+
+class field:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def calculate_cells(self):
+        print("Number: ", self.height*self.width)

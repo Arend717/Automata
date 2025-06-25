2cd2b651618465fb154d4ebeeb14c1c9cb42e4a8
saarpie <saartjepieterse@gmail.com>
2025-06-25 12:08:38 +0200

    editted rule 30 rules

---
+++ rule30_rule.py
@@ -0,0 +1,20 @@
+# rule30_rule.py
+
+def rule30(cell_position, state, neighbor_states):
+    """
+    Implements Rule 30 using (left, center, right) as key.
+    Only used in 1D cellular automata.
+    """
+    rule_30 = {
+        (1, 1, 1): 0,
+        (1, 1, 0): 0,
+        (1, 0, 1): 0,
+        (1, 0, 0): 1,
+        (0, 1, 1): 1,
+        (0, 1, 0): 1,
+        (0, 0, 1): 1,
+        (0, 0, 0): 0,
+    }
+
+    key = tuple(neighbor_states)
+    return rule_30.get(key, 0)
